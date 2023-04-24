47ddfd5e2ef35d237372f3b225d75f034e3189ee
Douglas Pérez <douglas.eduardo2000@gmail.com>
2023-04-24 11:13:41 -0400

    fix module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import { GetServerSideProps, NextPage } from 'next';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import BlueImage from '../assets/img/blue.jpg';
-import '../styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- types/pages/index.d.ts
+++ types/pages/index.d.ts
@@ -2,9 +2,12 @@ import { GetServerSideProps, NextPage } from 'next';
 
 declare const getServerSideProps: GetServerSideProps;
 
+export const getServerSideProps;
+
 interface Props {
   propTest: string;
 }
 
 declare const Home: NextPage<Props>;
-export { Home, getServerSideProps };
+
+export default Home;
